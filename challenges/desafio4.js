0825f42b1838dc951396b0330c2580bf8ad16b26
João Victor Schiavon <schiavonj@yahoo.com.br>
2023-03-15 15:58:45 -0300

    feat: requisite 04

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,4 @@
 db.produtos
-  .find({ $and: [{ vendido: { lte: 100 } }, { vendido: { gte: 50 } }] }, { _id: 0, nome: 1, })
+  .find({ $and: [{ vendidos: { $lt: 100 } }, { vendidos: { $gt: 50 } }] },
+    { _id: 0, nome: 1, vendidos: 1 })
   .sort({ vendidos: 1 });
